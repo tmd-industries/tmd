8163567dd5fae354b75e04430d0953b55f2a548d
badisa <badisa@jank.com>
2025-09-23 12:28:04 -0600

    Fix copyright

--- tests/test_potential_executor.py
+++ tests/test_potential_executor.py
@@ -1,4 +1,4 @@
-# Copyright 2019-2025, Relay Therapeutics
+# Copyright 2025, Forrest York
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
