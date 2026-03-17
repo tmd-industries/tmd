276f8847b4cd6cb4fe4187fbba9024777febca9d
badisa <badisa@jank.com>
2026-03-17 16:48:41 -0600

    Highlight the weakened torsions
    * Lots of the molecules appear weakened, though intramolecular weakening
      is different

--- tmd/fe/rest/plots.py
+++ tmd/fe/rest/plots.py
@@ -6,7 +6,11 @@ from tmd.fe.utils import get_mol_name
 
 
 def plot_rest_region(single_top: SingleTopologyREST) -> Draw.MolsToGridImage:
-    """Generate an SVG grid of a pair of molecules from a SingleToplogyREST object."""
+    """Generate an SVG grid of a pair of molecules from a SingleToplogyREST object.
+
+    Atoms and bonds that are in the REST region are highlighted in red, while torsions that are softened
+    are in blue.
+    """
     assert isinstance(single_top, SingleTopologyREST), "Must provide SingleTopologyREST object"
 
     mol_a_idxs, mol_b_idxs = single_top.split_combined_idxs(single_top.base_rest_region_atom_idxs)
@@ -22,12 +26,25 @@ def plot_rest_region(single_top: SingleTopologyREST) -> Draw.MolsToGridImage:
     AllChem.Compute2DCoords(mol_b)
 
     alchemical_rest_idxs = single_top.rest_region_atom_idxs
+
     mol_a_idxs = [a for a, c in enumerate(single_top.a_to_c) if c in alchemical_rest_idxs]
     mol_b_idxs = [b for b, c in enumerate(single_top.b_to_c) if c in alchemical_rest_idxs]
 
-    highlight_color = (238 / 255, 144 / 255, 144 / 255)  # red
+    weakened_torsion_idxs = set()
+    # Include the proper torsions that are weakened
+    for proper in single_top.target_propers.values():
+        for idx in proper.idxs:
+            if idx not in alchemical_rest_idxs:
+                weakened_torsion_idxs.add(idx)
 
-    def generate_rest_bond_idxs_and_colors(mol, rest_atoms):
+    # print(weakened_torsion_idxs)
+    rest_region_color = (238 / 255, 144 / 255, 144 / 255)  # red
+    weakened_torsion_color = (144 / 255, 144 / 255, 200 / 255)  # blue
+
+    mol_a_weakened_torsions = [a for a, c in enumerate(single_top.a_to_c) if c in weakened_torsion_idxs]
+    mol_b_weakened_torsions = [b for b, c in enumerate(single_top.b_to_c) if c in weakened_torsion_idxs]
+
+    def generate_rest_bond_idxs_and_colors(mol, rest_atoms, weakened_torsions):
         bond_idxs = []
         bond_colors = dict()
         rest_atoms_set = set(rest_atoms)
@@ -35,16 +52,23 @@ def plot_rest_region(single_top: SingleTopologyREST) -> Draw.MolsToGridImage:
             src_a, dst_a = bond.GetBeginAtomIdx(), bond.GetEndAtomIdx()
             if src_a in rest_atoms_set and dst_a in rest_atoms_set:
                 bond_idxs.append(bond.GetIdx())
-                bond_colors[int(bond.GetIdx())] = highlight_color
+                if src_a in weakened_torsions and dst_a in weakened_torsions:
+                    bond_colors[int(bond.GetIdx())] = weakened_torsion_color
+                else:
+                    bond_colors[int(bond.GetIdx())] = rest_region_color
 
         return bond_idxs, bond_colors
 
-    atom_colors_a = {int(idx): highlight_color for idx in mol_a_idxs}
-    atom_colors_b = {int(idx): highlight_color for idx in mol_b_idxs}
+    atom_colors_a = {int(idx): rest_region_color for idx in mol_a_idxs}
+    atom_colors_a.update({int(idx): weakened_torsion_color for idx in mol_a_weakened_torsions})
+    atom_colors_b = {int(idx): rest_region_color for idx in mol_b_idxs}
+    atom_colors_b.update({int(idx): weakened_torsion_color for idx in mol_b_weakened_torsions})
+    mol_a_idxs.extend(list(mol_a_weakened_torsions))
+    mol_b_idxs.extend(list(mol_b_weakened_torsions))
 
     # highlight bond idxs
-    bond_idxs_a, bond_colors_a = generate_rest_bond_idxs_and_colors(mol_a, mol_a_idxs)
-    bond_idxs_b, bond_colors_b = generate_rest_bond_idxs_and_colors(mol_b, mol_b_idxs)
+    bond_idxs_a, bond_colors_a = generate_rest_bond_idxs_and_colors(mol_a, mol_a_idxs, mol_a_weakened_torsions)
+    bond_idxs_b, bond_colors_b = generate_rest_bond_idxs_and_colors(mol_b, mol_b_idxs, mol_b_weakened_torsions)
 
     hals = [mol_a_idxs, mol_b_idxs]
     hacs = [atom_colors_a, atom_colors_b]
