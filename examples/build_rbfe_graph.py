30d57455c60585755e4cd87726677e9316242ca7
badisa <badisa@jank.com>
2026-07-13 14:02:36 -0600

    Document heavy_matches_heavy_only as required for constraints

--- examples/build_rbfe_graph.py
+++ examples/build_rbfe_graph.py
@@ -228,6 +228,8 @@ def main():
             continue
         help_str = f"Value for Atom Mapping argument {arg}"
         if isinstance(val, bool):
+            if arg == "heavy_matches_heavy_only":
+                help_str += ". Must be enabled to run with a timestep larger than 2.5fs."
             parser.add_argument(f"--atom_map_{arg}", type=int, choices=[0, 1], default=1 if val else 0, help=help_str)
         elif isinstance(val, (int, float)):
             parser.add_argument(f"--atom_map_{arg}", type=type(val), default=val, help=help_str)
